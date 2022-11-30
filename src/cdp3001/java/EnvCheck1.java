378465bf68c52c7d18d4785a1ec1f5e0595cc825
Amrit Chhetri <amrit.chhetrib@roserinchcs.com>
2022-11-30 13:17:47 +0530

    first commit

---
+++ src/cdp3001/java/EnvCheck1.java
@@ -0,0 +1,9 @@
+package cdp3001.java;
+public class EnvCheck1 {
+    public static void main(String[] args){
+        for( int i=0; i<=4; i++){
+            System.out.println("Value" +1);
+        }
+    }
+
+}
---
+++ src/cdp3001/java/database/MySQLConnectivity.java
@@ -0,0 +1,29 @@
+package cdp3001.java.database;
+import java.sql.*;
+
+public class MySQLConnectivity {
+    public static void main(String arg[]) {
+        Connection conObj = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conObj = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/cdpdb",
+                    "root", "");
+
+            Statement statement;
+            statement = conObj.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery(
+                    "select * from rdddata");
+            while (resultSet.next()) {
+
+                System.out.println(resultSet.getInt(1));
+            }
+            resultSet.close();
+            statement.close();
+            conObj.close();
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+    }
+}
